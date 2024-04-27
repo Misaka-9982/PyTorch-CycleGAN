80266e403670ab155e136492f000e0584af5c8ab
Mikoto Misaka <1355016743@qq.com>
2024-04-27 23:16:44 +0800

    绘制损失函数折线图以及训练中效果输出

--- utils.py
+++ utils.py
@@ -2,18 +2,22 @@ import random
 import time
 import datetime
 import sys
+from math import ceil
+from os import makedirs
 
 from torch.autograd import Variable
 import torch
 # from visdom import Visdom
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 
 def tensor2image(tensor):
-    image = 127.5 * (tensor[0].cpu().float().numpy() + 1.0)
+    image = 127.5 * (tensor[0].cpu().float().numpy() + 1.0)  # 从[-1,1]转换为[0,2]再转换为[0,255]
     if image.shape[0] == 1:
-        image = np.tile(image, (3, 1, 1))
+        image = np.tile(image, (3, 1, 1))  # 灰度图复制为3个通道
+    image = np.transpose(image, (1, 2, 0))  # 转为HWC的顺序
     return image.astype(np.uint8)
 
 
@@ -27,8 +31,10 @@ class Logger():
         self.prev_time = time.time()
         self.mean_period = 0
         self.losses = {}
+        self.losses_history: {str: list} = {}
         self.loss_windows = {}
         self.image_windows = {}
+        makedirs('./output/train_output', exist_ok=True)
 
     def log(self, losses=None, images=None):
         self.mean_period += (time.time() - self.prev_time)
@@ -52,30 +58,46 @@ class Logger():
         batches_left = self.batches_epoch * (self.n_epochs - self.epoch) + self.batches_epoch - self.batch
         sys.stdout.write('ETA: %s' % (datetime.timedelta(seconds=batches_left * self.mean_period / batches_done)))
 
-        # 每十代保存一次结果
-
-        for image_name, tensor in images.items():
-            # 使用visdom服务器输出训练信息时
-            # if image_name not in self.image_windows:
-            # self.image_windows[image_name] = self.viz.image(tensor2image(tensor.data), opts={'title':image_name})
-            # else:
-            # self.viz.image(tensor2image(tensor.data), win=self.image_windows[image_name], opts={'title':image_name})
-            # 直接输出到文件时
-            Image.fromarray(tensor2image(tensor.data)).save(f'./output/{image_name}_' + f'epoch{self.epoch}.jpg')
-        # End of epoch
+        # 每十代保存一次图片结果
+        if self.epoch % 10 == 0 and self.batch == 1:
+            for image_name, tensor in images.items():
+                # 使用visdom服务器输出训练信息时
+                # if image_name not in self.image_windows:
+                # self.image_windows[image_name] = self.viz.image(tensor2image(tensor.data), opts={'title':image_name})
+                # else:
+                # self.viz.image(tensor2image(tensor.data), win=self.image_windows[image_name], opts={'title':image_name})
+                # 直接输出到文件时
+                Image.fromarray(tensor2image(tensor.data)).save(f'./output/train_output/{image_name}_' + f'epoch{self.epoch}.jpg')
+        # epoch结束时保存损失函数图像
         if (self.batch % self.batches_epoch) == 0:
-            # Plot losses
+            # 保存损失函数历史
             for loss_name, loss in self.losses.items():
-                if loss_name not in self.loss_windows:
-                    # self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]),
-                    #                                                 opts={'xlabel': 'epochs', 'ylabel': loss_name, 'title': loss_name})
-                    pass
+                # 文件损失函数图输出
+                if loss_name not in self.losses_history:
+                    self.losses_history[loss_name] = [loss]
                 else:
-                    # self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), win=self.loss_windows[loss_name], update='append')
-                    pass
-                # Reset losses for next epoch
+                    self.losses_history[loss_name].append(loss)
+                # visdom输出
+                # if loss_name not in self.loss_windows:
+                #     # self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]),
+                #     #                                                 opts={'xlabel': 'epochs', 'ylabel': loss_name, 'title': loss_name})
+                #     pass
+                # else:
+                #     # self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), win=self.loss_windows[loss_name], update='append')
+                #     pass
+                # 为下一代重置loss
                 self.losses[loss_name] = 0.0
 
+            # 绘制图像
+            loss_fig, loss_ax = plt.subplots(ceil(len(self.losses_history)/3), 3)
+            loss_ax = loss_ax.reshape(-1)  # 二维变一维
+            for i, (loss_name, loss) in enumerate(self.losses_history.items()):
+                loss_ax[i].plot(range(len(loss)), loss)
+                loss_ax[i].set_xlabel('Epoch')
+                loss_ax[i].set_ylabel(loss_name)
+                loss_ax[i].set_title(loss_name)
+            loss_fig.tight_layout()  # 调整布局
+            plt.savefig('./output/train_output/losses_curve.jpg')
             self.epoch += 1
             self.batch = 1
             sys.stdout.write('\n')
